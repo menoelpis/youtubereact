11133ec7ec6be73294b0b972725d539bcda39730
Daniel Park <abideinhope@gmail.com>
2018-06-16 22:50:09 +0900

    Changed Functional Component to Classbased Component

--- src/index.js
+++ src/index.js
@@ -1,9 +1,7 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
-
 import SearchBar from './components/search_bar';
-
 const API_KEY = 'AIzaSyBI1QLjvSF6BFN24K5x37w5H2POXj5HsP8';
 
 YTSearch({key: API_KEY, term: 'KJV Bible'}, function(data) {
@@ -11,12 +9,14 @@ YTSearch({key: API_KEY, term: 'KJV Bible'}, function(data) {
 });
 
 // Create a new component, This component should produce some HTML
-const App = () => {
-  return (
-    <div>
-      <SearchBar />
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <SearchBar />
+      </div>
+    );
+  }
 }
 
 // Take this component's generated HTML and put it on the page (in the DOM)
